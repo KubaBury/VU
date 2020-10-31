05127ec43518097b8f11b63781f49a680e45cba0
KubaBury <bureja11@seznam.cz>
2020-11-01 00:24:40 +0100

    more comment added

--- SCRIPTS/testing.jl
+++ SCRIPTS/testing.jl
@@ -1,12 +1,12 @@
-using DelimitedFiles, Mill, StatsBase, Flux
+using DelimitedFiles, Mill, StatsBase
 using FileIO, JLD2, Statistics
-using Flux: throttle, @epochs
+using Flux: train!
 using Mill: reflectinmodel
 using Base.Iterators: repeated
-using Plots
-using Random
+using Plots: plot
+using Random: shuffle
 
-data = "D:/VU/SCRIPTS_/DataSets/Tiger";
+data = "D:/VU/SCRIPTS/DataSets/Tiger";
 ###BrownCreeper, CorelAfrican,CorelBeach, Elephant,Fox, Musk1, Musk2, Mutagenesis1(2)
 ###Newsgroups1, Newsgroups2, Newsgroups3, Protein, Tiger, Web1(2,3,4), WinterWren
 
@@ -32,17 +32,17 @@ y_oh = Flux.onehotbatch((y.+1)[:],1:2);
 
 ###### cross-validation - specify number of folds and dense layer ###### 
 
-W = 50
-h = 1
+W = 100 #dense layer
+h = 1 #step in dense layer
 b = length(y); #number of bags
 A = length(x.data.data[:,1]); # for dense layer
-L_valid = zeros(W,1); # initial position of loss matrix
-L_test1 = zeros(W,1)
-L_test2 = zeros(W,1)
+L_valid = zeros(W,1); #vector for loss values of validation set
+L_test1 = zeros(W,1); #vector for loss values of test set (1)
+L_test2 = zeros(W,1); #vector for loss values of test set (2)
     
-opt = Flux.ADAM(0.0001)
+opt = Flux.ADAM();
 
-### define random training and validation samples
+
     
 for i = 1:W
 	# create the model
@@ -50,7 +50,9 @@ for i = 1:W
     ArrayModel(Dense(A, h*i, Flux.tanh)),                      		# model on the level of Flows
     SegmentedMeanMax(h*i),                                      		# aggregation
     ArrayModel(Chain(Dense(2*h*i, h*i, Flux.tanh), Dense(h*i, 2)))) ; 	# model on the level of bags
-    
+	
+	### define random training and validation samples
+
     l1 = sample(1:100, 80, replace = false)
     l2 = sample(101:200, 5, replace = false)
     a1 = sample(symdiff(1:100, l1), 8, replace = false)
@@ -67,7 +69,7 @@ for i = 1:W
 	loss(x, y_oh) = Flux.logitcrossentropy(model(x).data, y_oh);
 	#train
 	Flux.train!(loss, params(model), repeated((x[train_set], y_oh[:,train_set]), 1000), opt);
-	# calculate loss for each fold and dense layer
+	# calculate loss for each set and dense layer
 	L_valid[i] = loss(x[validation_set], y_oh[:,validation_set])
     L_test1[i] = loss(x[test_set1], y_oh[:,test_set1])
     L_test2[i] = loss(x[test_set2], y_oh[:,test_set2])
@@ -77,6 +79,8 @@ L_valid;
 L_test1;
 L_test2;
 
+#plot all together
+
 p1 = plot(1:W, L_valid, xlabel = "dense layer", ylabel = "loss", label = "validation set", legend =:topleft, color =:red, linewidth = 4 );
-p2 = plot!(p1, 1:W, L_test1, label = "test set (1)", color = :blue, linewidth = 4, line =:dash);
-p3 = plot!(p2, 1:W, L_test2, label = "test set (2)", color = :green, linewidth = 4 , line =:dot)
+p2 = plot!(p1, 1:W, L_test1, label = "test set (1)", color = :blue, linewidth = 4);
+p3 = plot!(p2, 1:W, L_test2, label = "test set (2)", color = :green, linewidth = 4)
