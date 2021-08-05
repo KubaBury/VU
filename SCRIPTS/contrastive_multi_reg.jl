b2582b956a3c36f759cbb88fcf4c2f1910a387b8
KubaBury <bureja11@seznam.cz>
2021-08-05 23:02:20 +0200

    logsumexp

--- SCRIPTS/contrastive_multi_reg.jl
+++ SCRIPTS/contrastive_multi_reg.jl
@@ -9,46 +9,38 @@ using LinearAlgebra
 #generate data and set known parameters
 σ = 0.2
 τ = 1.0
-α = 0:0.1:1
-
-n=9
-Θ=zeros(n+1,length(α))
-L= zeros(length(α))
-
+n=5 #degree of the polynom
 x1 = rand(Normal(1,σ^2*τ^2), 5)
 x2 = rand(Normal(2,σ^2*τ^2), 5)
-
-x = vcat(x1,x2)
-W = [(x).^o for o = 0:n]
-X = hcat(W...)
-θ = zeros(n+1)
+x = vcat(x1,x2) 
 y = 2 .*x .+ 1 + rand(Normal(0, 0.2), 10)
-x3 = 0.8:0.01:2.2
-W2 = [(x3).^i for i = 0:n]
-X2 = hcat(W2...)
+α = 0:0.1:1
+Θ = zeros(n+1,length(α)) #preallocated space 
+θ = zeros(n+1) #preallocated space 
 
-#model
-f(θ,x,y) =  exp.( .-(((y'.- X*θ).^2) ./(2σ^2)) .- ((x.^2)./(2σ^2τ^2)) )
-
-    for alpha = 1:11
-     #first q-factor with sum over y
-        q1(θ,x,y) = α[alpha]*diag(f(θ,x,y)) .-
-        .- (α[alpha]*log.(sum(f(θ,x,y), dims=2)))'
+#define model
+f(θ,x,y) =  .-(((y'.- hcat([(x).^o for o = 0:n]...)*θ).^2) ./(2σ^2)) .- ((x.^2)./(2σ^2τ^2)) 
+    
+    
+for alpha = 1:11 #cycle over different alpha
+        #first q-factor with sum over y
+        q1(θ,x,y) = α[alpha]*diag(exp.(f(θ,x,y))) .-
+        .- (α[alpha]*logsumexp(f(θ,x,y), dims=2))'
 
         #second q-factor with sum over x
-        q2(θ,x,y) = (1-α[alpha])*diag(f(θ,x,y)) .-
-        .- (1-α[alpha])*log.(sum(f(θ,x,y), dims=1))
+        q2(θ,x,y) = (1-α[alpha])*diag(exp.(f(θ,x,y))) .-
+        .- (1-α[alpha])*logsumexp(f(θ,x,y), dims=1)
 
-        #all together with E_{p_data} ?
+        #all together with E_{p_data} -> define loss
         loss(x,y,θ) = -mean(q1(θ,x,y) + q2(θ,x,y))
 
         #train
         data = Iterators.repeated((x,y),10000)
-        Flux.train!((x,y)->loss(x,y,θ), [θ],data, ADAM())
+        Flux.train!((x,y)->loss(x,y,θ),[θ],data,ADAM())
 
         #save trained parameters for plot
         Θ[:,alpha] = θ
-    end
+ end
 
 
 #Plots
@@ -58,7 +50,9 @@ x3 = 0.9:0.01:2.1
 W2 = [(x3).^i for i = 0:n]
 X2 = hcat(W2...)
 Y2 = X2*Θ
-ψ = inv(X'*X)*X'*y
+#ψ = inv(X'*X)*X'*y unstable
+X=hcat([(x).^o for o = 0:n]...)
+ψ1 = pinv(X'*X)*X'*y #Moore-Penrose inverse
 
 plot!(x3,Y2, 
     label=[L"$\alpha=0.0$" L"$\alpha=0.1$" L"$\alpha=0.2$" L"$\alpha=0.3$" L"$\alpha=0.4$" L"$\alpha=0.5$" L"$\alpha=0.6$" L"$\alpha=0.7$" L"$\alpha=0.8$" L"$\alpha=0.9$" L"$\alpha=1.0$"],
@@ -67,5 +61,5 @@ plot!(x3,Y2,
     linestyle=[:solid :dash :dash :dash :dash :solid :dash :dash :dash :dash :solid],
     legend=:topleft)
 Θ
-plot!(x3,X2*ψ, label=L"simple~reg", lw = 3, color=:purple)
+plot!(x3,X2*ψ1, label=L"simple~reg", lw = 3, color=:purple)
 
