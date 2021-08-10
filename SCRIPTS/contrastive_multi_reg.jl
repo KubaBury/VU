da594cf6e1df2cf6740615d1968d2c7db5eced43
KubaBury <bureja11@seznam.cz>
2021-08-10 17:39:25 +0200

    least squares loss

--- SCRIPTS/contrastive_multi_reg.jl
+++ SCRIPTS/contrastive_multi_reg.jl
@@ -7,13 +7,13 @@ using LinearAlgebra
 
 
 #generate data and set known parameters
-σ = 0.2
+σ = 0.25
 τ = 1.0
 n=5 #degree of the polynom
 x1 = rand(Normal(1,σ^2*τ^2), 5)
 x2 = rand(Normal(2,σ^2*τ^2), 5)
 x = vcat(x1,x2) 
-y = 2 .*x .+ 1 + rand(Normal(0, 0.2), 10)
+y = 3 .*x .+ 1 + rand(Normal(0, σ^2*τ^2), 10)
 α = 0:0.1:1
 Θ = zeros(n+1,length(α)) #preallocated space 
 θ = zeros(n+1) #preallocated space 
@@ -22,24 +22,24 @@ y = 2 .*x .+ 1 + rand(Normal(0, 0.2), 10)
 f(θ,x,y) =  .-(((y'.- hcat([(x).^o for o = 0:n]...)*θ).^2) ./(2σ^2)) .- ((x.^2)./(2σ^2τ^2)) 
     
     
-for alpha = 1:11 #cycle over different alpha
-        #first q-factor with sum over y
-        q1(θ,x,y) = α[alpha]*diag(exp.(f(θ,x,y))) .-
-        .- (α[alpha]*logsumexp(f(θ,x,y), dims=2))'
+for alfa = 1:11 #cycle over different alpha
+        #first q-factor for least squares -> L2 norm
+        q1(θ,x,y) = α[alfa]*sum((y.- hcat([(x).^o for o = 0:n]...)*θ).^2) #.-
+       # .- (α[alfa]*logsumexp(f(θ,x,y), dims=2))'
 
         #second q-factor with sum over x
-        q2(θ,x,y) = (1-α[alpha])*diag(exp.(f(θ,x,y))) .-
-        .- (1-α[alpha])*logsumexp(f(θ,x,y), dims=1)
+        q2(θ,x,y) = (1-α[alfa])*diag((f(θ,x,y))) .-
+        .- ((1-α[alfa])*logsumexp(f(θ,x,y), dims=1))'
 
         #all together with E_{p_data} -> define loss
-        loss(x,y,θ) = -mean(q1(θ,x,y) + q2(θ,x,y))
+        loss(x,y,θ) = q1(θ,x,y) .+ -mean(q2(θ,x,y))
 
         #train
         data = Iterators.repeated((x,y),10000)
         Flux.train!((x,y)->loss(x,y,θ),[θ],data,ADAM())
 
         #save trained parameters for plot
-        Θ[:,alpha] = θ
+        Θ[:,alfa] = θ
  end
 
 
@@ -61,5 +61,5 @@ plot!(x3,Y2,
     linestyle=[:solid :dash :dash :dash :dash :solid :dash :dash :dash :dash :solid],
     legend=:topleft)
 Θ
-plot!(x3,X2*ψ1, label=L"simple~reg", lw = 3, color=:purple)
+#plot!(x3,X2*ψ1, label=L"simple~reg", lw = 3, color=:purple)
 
