b71ed7782d21881d1dfdda6b0d2476b7d0e3772b
KubaBury <bureja11@seznam.cz>
2021-08-27 15:01:26 +0200

    update

--- SCRIPTS/cross_validation_randomized.jl
+++ SCRIPTS/cross_validation_randomized.jl
@@ -6,7 +6,7 @@ using Base.Iterators: repeated
 using Random: shuffle
 using Plots
 
-data = "D:/VU/SCRIPTS_/DataSets/Tiger"
+data = "D:/VU/SCRIPTS/DataSets/Tiger"
 ###BrownCreeper, CorelAfrican,CorelBeach, Elephant,Fox, Musk1, Musk2, Mutagenesis1(2)
 ###Newsgroups1, Newsgroups2, Newsgroups3, Protein, Tiger, Web1(2,3,4), WinterWren
 
@@ -30,46 +30,63 @@ end
 y_oh = Flux.onehotbatch((y.+1)[:],1:2);
 
 ### cross-validation - specify number of folds and dense layer ###### 
+K=5
+W=20
+h=1
+A = length(x.data.data[:,1]) # for dense layer
+b = length(y) #number of bags
+#function KfoldCV(K,W;h=1)
 
-function KfoldCV(K,W,h)
-	b = length(y) #number of bags
-	A = length(x.data.data[:,1]) # for dense layer
 	n = floor(Int, length(y)/K) # number of bags in each fold
 	L1 = zeros(W,K) # initial position of loss array
-	train_sets = zeros(Int, b-n, K)
-	validation_sets = zeros(Int, n, K)
+	L2 = zeros(W,K)
+	L3 = zeros(K)
+	L4 = zeros(K)
+	tr_set = zeros(Int, b-n, K)
+	te_set = zeros(Int, n, K)
 
 	opt = Flux.ADAM();
 
-	### define random training and validation samples
-	for j = 1:K
+	### define random training and testing samples
+	for  j =1:K
 		r1 = shuffle(1:b)
 		r2 = sample(1:b, n, replace = false)
 		q = symdiff(r1, r2)
-		train_sets[:,j] = q 
-		validation_sets[:,j] = r2
+		tr_set[:,j] = q 
+		te_set[:,j] = r2
 	end
-
 	for i = 1:W
 		# create the model
 		model = BagModel(
     	ArrayModel(Dense(A, h*i, Flux.tanh)),                      			# model on the level of Flows
-    	SegmentedMeanMax(h*i),                                      		# aggregation
-		ArrayModel(Chain(Dense(2*h*i, h*i, Flux.tanh), Dense(h*i, 2)))) ; 	# model on the level of bags
+    	meanmax_aggregation(h*i),                                      		# aggregation
+		ArrayModel(Chain(Dense(2*h*i+1, h*i, Flux.tanh), Dense(h*i, 2)))) ; 	# model on the level of bags
 	
 		#loss function
 		loss(x, y_oh) = Flux.logitcrossentropy(model(x).data, y_oh);
 
-		for j = 1:K
-			#train
-			Flux.train!(loss, params(model), repeated((x[train_sets[:,j]], y_oh[:,train_sets[:,j]]), 1000), opt);
+		for  m = 1:K
+		#train
+		Flux.train!(loss, Flux.params(model), repeated((x[tr_set[:,m]], y_oh[:,tr_set[:,m]]), 1000), opt);
 			# calculate loss for each fold and dense layer
-			L1[i,j] = loss(x[validation_sets[:,j]], y_oh[:,validation_sets[:,j]])
-		end;
-	end;
+			L1[i,m] = loss(x[te_set[:,m]], y_oh[:,te_set[:,m]])
+			L2[i,m] = loss(x[tr_set[:,m]], y_oh[:,tr_set[:,m]])
+			L3[m] =	1-mean(mapslices(argmax, model(x[tr_set[:,m]]).data, dims=1)' .!= y[tr_set[:,m]].+1)
+			L4[m] = 1-mean(mapslices(argmax, model(x[te_set[:,m]]).data, dims=1)' .!= y[te_set[:,m]].+1)
+	end
+end
 	#visualization
 	x2 = mean(L1, dims = 2)
+	x3 = mean(L2, dims = 2)
 	x1 = 1:W
-	plot(x1, x2, xlabel = "dense layer", ylabel = "loss", legend = false)
-end
+	plot(x1, x2, xlabel = "Model Complexity", ylabel = "Prediction Error", 
+	label = "Testing data",lw=3,
+	legend=:topleft, color=:blue, title="KfoldCV, Tiger",
+	xtickfont=font(11), 
+    ytickfont=font(11),
+    guidefont=font(14),
+    legendfont=font(11))
+	plot!(x1, x3,label = "Training data", lw=3, legend=:topleft, color=:red)
+	mean(L4)
+#end
 
--- SCRIPTS/testing.jl
+++ SCRIPTS/testing.jl
@@ -23,87 +23,63 @@ function csv2mill(problem)
 	(samples = BagNode(ArrayNode(x), bags), labels = y)
 end
 
-data = "D:/VU/SCRIPTS/DataSets/Musk1"
+data = "D:/VU/SCRIPTS/DataSets/Tiger"
 (x,y) = csv2mill(data)
+
+
 K=5
-	b = length(y) #number of bags
-	n = floor(Int, length(y)/K); # number of bags in each fold
-	train_sets = zeros(Int, b-n, K);
-	test_sets = zeros(Int, n, K);
-
-	opt = Flux.ADAM()
-
-	### define random training and validation samples
-	for j = 1:K
-		l1 = (1:b)
-		l2 = (1+n*(j-1):n+n*(j-1))
-		q = symdiff(l1,l2)
-		train_sets[:,j] = q
-		test_sets[:,j] = l2
-	end
-
-
-
-x_train=x[1:52]	
-y_train=y[1:52]
-x_test=x[53:92]	
-y_test=y[53:92]
-y_ohtrain = Flux.onehotbatch((y_train.+1)[:],1:2)
-y_ohtest = Flux.onehotbatch((y_test.+1)[:],1:2)
-A = length(x.data.data[:,1])
-# create the model
- # model on the level of bags
-
-
-#function logitcrossentropy(ŷ, y; dims = 1, agg = mean)
-#    agg(.-sum(y .* logsoftmax(ŷ; dims = dims); dims = dims))
-#end
-
-
-#function hybridloss1(ŷ, y, α; agg=mean)
-#	agg(.-α.*sum(y .* logsoftmax(ŷ; dims = 1); dims = 1) .- (1-α).* sum(y .* logsoftmax(ŷ; dims = 2); dims = 1))
-#end
+A = length(x.data.data[:,1]) # for dense layer
+b = length(y) #number of bags
+n = floor(Int, length(y)/K) # number of bags in each fold
+L1 = zeros(W,K) # initial position of loss array
+L2 = zeros(W,K)
+L3 = zeros(K)
+L4 = zeros(K)
+tr_set = zeros(Int, b-n, K)
+te_set = zeros(Int, n, K)
+### define random training and testing samples
+for  j =1:K
+	r1 = shuffle(1:b)
+	r2 = sample(1:b, n, replace = false)
+	q = symdiff(r1, r2)
+	tr_set[:,j] = q 
+	te_set[:,j] = r2
+end
 
 # define loss function as a combination of standard cross entropy loss and end-to-end version of 
 #contrastive learning a.k.a. equation 2.14. Parameter α is set at the begining of the sript.
 
+loss(x, y_oh) = Flux.logitcrossentropy(model(x).data, y_oh)
+
 function hybridloss(ŷ, y, α; agg=mean)
-	 agg(.-sum( α.*(y .* logsoftmax(ŷ; dims = 1)) + (1-α).*(y .* logsoftmax(ŷ; dims = 2)); dims = 1))   
-end
+	agg(.-sum( α.*(y .* logsoftmax(ŷ; dims = 1)) + (1-α).*(y .* logsoftmax(ŷ; dims = 2)); dims = 1))   
+end 
 
-
-#loss(x, y_oh) = Flux.logitcrossentropy(model(x).data, y_oh)
-
-	
-
-# the usual way of training
-#evalcb = throttle(() -> @show(loss1(x, y_oh)), 1)
 opt = Flux.ADAM()
-
 α=0:0.1:1
-H=zeros(length(α))
-E=zeros(length(α))
+H=zeros(length(α), K)
+E=zeros(length(α), K)
 
-for a = 11:-1:1
-	function hybridloss(ŷ, y, α; agg=mean)
-		agg(.-sum( α.*(y .* logsoftmax(ŷ; dims = 1)) + (1-α).*(y .* logsoftmax(ŷ; dims = 2)); dims = 1))   
-	end 
-
-	model = BagModel(
+for a = 1:11
+	for j = 1:K
+		model = BagModel(
     ArrayModel(Dense(A, 10, Flux.tanh)),                      # model on the level of Flows
     meanmax_aggregation(10),                                       # aggregation
     ArrayModel(Chain(Dense(21, 10, Flux.tanh), Dense(10, 2)))) 
-
-loss1(x,y_oh) = hybridloss(model(x).data, y_oh, α[a])
-Flux.train!(loss1, Flux.params(model), repeated((x_train, y_ohtrain), 1000), opt)#,cb=evalcb)
-H[a]=loss1(x_test, y_ohtest)
-E[a]=loss1(x_train,y_ohtrain)
-
+	loss1(x,y_oh) = hybridloss(model(x).data, y_oh, α[a])
+	Flux.train!(loss1, Flux.params(model), repeated((x[tr_set[:,j]], y_oh[:, tr_set[:,j]]), 1000), opt)#,cb=evalcb)
+	H[a,j]=loss(x[te_set[:,j]], y_oh[:, te_set[:,j]])
+	E[a,j]=loss(x[tr_set[:,j]], y_oh[:, tr_set[:,j]])
+	end
 end
 
-H
-E
-plot(α, H, lw=3, color=:red, xlabel=L"$\alpha$", ylabel="loss", label="Testing data"	)
-plot!(α, E, color=:blue, lw=3, label="Training data")
+plot(α, mean(H, dims=2), lw=3, color=:red, xlabel=L"$\alpha$", ylabel="Prediction Error", label="Testing data",
+xtickfont=font(11), 
+    ytickfont=font(11),
+    guidefont=font(14),
+    legendfont=font(11)	)
+plot!(α, mean(E, dims=2), color=:blue, lw=3, label="Training data")
+
 # calculate the error on the training set (no testing set right now)
-#mean(mapslices(argmax, model(x).data, dims=1)' .!= y.+1)
+
+mean(mapslices(argmax, model(x).data, dims=1)' .!= y.+1)
